49a67d54ffc6e546e806950d2acb1a1c5b5461c0
Mike Gallagher <mikesligo@gmail.com>
2013-10-19 16:50:49 +0100

    moved to func

--- lab1.cpp
+++ lab1.cpp
@@ -14,30 +14,25 @@ Mat load_image(int argc, char**argv)
     return image;
 }
 
-int main( int argc, char** argv )
+Mat calc_hist(Mat src)
 {
-  Mat src;
-
-  cvtColor(load_image(argc, argv), src, CV_BGR2HSV);
-
   vector<Mat> bgr_planes;
   split( src , bgr_planes );
 
   /// Establish the number of bins
   int histSize = 256;
 
-  /// Set the ranges ( for B,G,R) )
   float range[] = { 0, 256 } ;
   const float* histRange = { range };
 
   bool uniform = true; bool accumulate = false;
 
-  Mat b_hist, g_hist, r_hist;
+  Mat hue, luminance, saturation;
 
   /// Compute the histograms:
-  calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-  calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-  calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[0], 1, 0, Mat(), hue, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[1], 1, 0, Mat(), luminance, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[2], 1, 0, Mat(), saturation, 1, &histSize, &histRange, uniform, accumulate );
 
   // Draw the histograms for B, G and R
   int hist_w = 512; 
@@ -47,25 +42,34 @@ int main( int argc, char** argv )
   Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
 
   /// Normalize the result to [ 0, histImage.rows ]
-  normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-  normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-  normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  normalize(hue, hue, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  normalize(luminance, luminance, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  normalize(saturation, saturation, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
 
   /// Draw for each channel
   for( int i = 1; i < histSize; i++ )
   {
-      /*line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-                       Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(hue.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(hue.at<float>(i)) ),
                        Scalar( 255, 0, 0), 2, 8, 0  );
-      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-                       Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-                       Scalar( 0, 255, 0), 2, 8, 0  );*/
-      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-                       Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-                       Scalar( 0, 0, 255), 2, 8, 0  );
+      /*line( histImage, Point( bin_w*(i-1), hist_h - cvRound(luminance.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(luminance.at<float>(i)) ),
+                       Scalar( 0, 255, 0), 2, 8, 0  );
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(saturation.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(saturation.at<float>(i)) ),
+                       Scalar( 0, 0, 255), 2, 8, 0  );*/
   }
 
-  /// Display
+  return histImage;
+}
+
+int main( int argc, char** argv )
+{
+  Mat src, histImage;
+
+  cvtColor(load_image(argc, argv), src, CV_BGR2HLS);
+  histImage = calc_hist(src);
+
   namedWindow(argv[1], CV_WINDOW_AUTOSIZE );
   imshow(argv[1], histImage );
 
