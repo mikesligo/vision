e921205b81ae382c581790e1180abfd874b8ca1c
Mike Gallagher <mikesligo@gmail.com>
2013-11-11 16:45:12 +0000

    dividing into bottom half of each bottle

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -7,10 +7,17 @@
 using namespace std;
 using namespace cv;
 
+// Split the image into interesting parts (bottom half, into 5 bottles) by using the known position of the bottles
 vector<Mat> get_bottles(Mat image)
 {
     vector<Mat> bottles;
-    bottles.push_back(image);
+    Rect roi(0,image.rows/2-1, image.cols, image.rows/2);
+    Mat cropped = image(roi);
+    int divider = cropped.cols/5;
+    for (int i=0; i< cropped.cols-1; i = i + divider){
+        Rect bottle_pos(i, 0, divider, cropped.rows);
+        bottles.push_back(cropped(bottle_pos));
+    }
     return bottles;
 }
 
@@ -24,12 +31,13 @@ int main( int argc, char** argv )
         image = imread(filename, CV_LOAD_IMAGE_COLOR);
         vector<Mat> bottles = get_bottles(image);
         namedWindow("Lab2", CV_WINDOW_AUTOSIZE );
-        moveWindow("Lab2", 300, 300);
         for (vector<Mat>::iterator it = bottles.begin(); it != bottles.end(); it++) {
             image = Mat(*it);
             imshow("Lab2", image );
+            moveWindow("Lab2", 300, 300);
         }
         waitKey(0);
+        //cvtColor(cropped, grey_scale, CV_BGR2GRAY);
     }
     return 0;
 
