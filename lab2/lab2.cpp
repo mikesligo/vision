ae7a6605d1d3f4ea7a94c6991a5cd1d0e26e239e
Mike Gallagher <mikesligo@gmail.com>
2013-11-12 17:37:18 +0000

    using kmeans

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -7,6 +7,32 @@
 using namespace std;
 using namespace cv;
 
+Mat get_k_means(Mat image, int clusterCount, int iterations){
+    Mat samples(image.rows * image.cols, 3, CV_32F);
+    for( int y = 0; y < image.rows; y++ ){
+        for( int x = 0; x < image.cols; x++ ){
+            for( int z = 0; z < 3; z++){
+                samples.at<float>(y + x*image.rows, z) = image.at<Vec3b>(y,x)[z];
+            }   
+        }   
+    }
+
+    Mat labels;
+    Mat centers;
+    kmeans(samples, clusterCount, labels, TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), iterations, KMEANS_PP_CENTERS, centers );
+
+    Mat new_image( image.size(), image.type() );
+    for( int y = 0; y < image.rows; y++ ){
+        for( int x = 0; x < image.cols; x++ ){  
+            int cluster_idx = labels.at<int>(y + x*image.rows,0);
+            new_image.at<Vec3b>(y,x)[0] = centers.at<float>(cluster_idx, 0); 
+            new_image.at<Vec3b>(y,x)[1] = centers.at<float>(cluster_idx, 1); 
+            new_image.at<Vec3b>(y,x)[2] = centers.at<float>(cluster_idx, 2); 
+        }   
+    }
+    return new_image;
+}
+
 // Split the image into interesting parts (bottom half, into 5 bottles) by using the known position of the bottles
 vector<Mat> get_bottles(Mat image)
 {
@@ -15,9 +41,11 @@ vector<Mat> get_bottles(Mat image)
     Mat cropped = image(roi);
     int divider = cropped.cols/5;
     for (int i=0; i< cropped.cols-1; i = i + divider){
-        Mat grey_scale;
+        Mat grey_scale, k_means, section;
         Rect bottle_pos(i, 0, divider, cropped.rows);
-        cvtColor(cropped(bottle_pos), grey_scale, CV_BGR2GRAY);
+        section = cropped(bottle_pos);
+        k_means = get_k_means(section,3,3);
+        cvtColor(k_means, grey_scale, CV_BGR2GRAY);
         bottles.push_back(grey_scale);
     }
     return bottles;
